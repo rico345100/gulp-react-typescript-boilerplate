bc0d38562bfb2141cfef76b575060fb234ce733f
modernator <rico345100@gmail.com>
2017-05-25 18:19:33 +0900

    Bug fixed: Express server did not used port number that specified in config.json

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 "use strict";
 const express = require('express');
 const app = express();
+const config = require('./config.json');
 
 app.use(express.static(__dirname + '/build'));
 app.get('/', (req, res) => res.sendFile(__dirname + '/build/html/index.html'));
 
 app.get('/api', (req, res) => res.json({ message: 'helloworld' }));
 
-app.listen(3000, () => console.log('Express Server listen at port 3000...'));
+app.listen(config.port, () => console.log(`Express Server listen at port ${config.port}...`));
